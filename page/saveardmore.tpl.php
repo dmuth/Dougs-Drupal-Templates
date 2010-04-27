c00aa4fe88347695a3be572ee209054f5ca8ed82
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2010-04-27 03:19:18 +0000

    Added Save Ardmore Coalition template.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@12 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

---
+++ page/saveardmore.tpl.php
@@ -0,0 +1,108 @@
+<?php
+//
+// Print a note when we are in the dev site
+//
+if (strstr($directory, "-dev")) {
+	print "DEVELOPMENT TEMPLATE - If you think you shouldn't be seeing "
+		. "this, please contact <a href=\"/user/2\">Doug</a>.";
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
+
+<head>
+  <title><?php print $head_title ?></title>
+  <?php print $head ?>
+  <?php print $styles ?>
+  <?php print $scripts ?>
+<?php
+//
+// Base directory for stuff under this template
+//
+?>
+<script type="text/javascript" >
+var directory = "/<?php print $directory; ?>"; 
+</script>
+<?php
+//
+// jQuery cookie plugin.  The jquery_cookie module breaks.
+// See: http://drupal.org/node/203435 for details
+//
+?>
+<script type="text/javascript" src="/<?php print $directory; ?>/cookie.js"></script>
+  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyle Content in IE */ ?> </script>
+</head>
+
+<body>
+
+<table border="0" cellpadding="0" cellspacing="0" id="header">
+  <tr>
+    <td id="logo">
+      <?php if ($logo) { ?><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" /></a><?php } ?>
+      <?php if ($site_name) { ?><h1 class='site-name'><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><?php print $site_name ?></a></h1><?php } ?>
+      <?php if ($site_slogan) { ?><div class='site-slogan'><?php print $site_slogan ?></div><?php } ?>
+    </td>
+    <td id="menu">
+      <?php if (isset($secondary_links)) { ?><div id="secondary"><?php print theme('links', $secondary_links) ?></div><?php } ?>
+      <?php if (isset($primary_links)) { ?><div id="primary"><?php print theme('links', $primary_links) ?></div><?php } ?>
+      <?php print $search_box ?>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2"><div><?php print $header ?></div></td>
+  </tr>
+</table>
+
+<table border="0" cellpadding="0" cellspacing="0" id="content">
+  <tr>
+    <?php if ($left) { ?><td id="sidebar-left">
+      <?php print $left ?>
+    </td><?php } ?>
+    <td valign="top">
+      <div id="main">
+        <?php print $breadcrumb ?>
+        <h1 class="title"><?php print $title ?></h1>
+        <div class="tabs"><?php print $tabs ?></div>
+        <?php print $help ?>
+        <?php print $messages ?>
+        <?php print $content; ?>
+      </div>
+    </td>
+    <?php if ($right) { ?><td id="sidebar-right">
+      <?php print $right ?>
+    </td><?php } ?>
+  </tr>
+</table>
+
+
+<div id="footer">
+
+    <div id="footer-region" class="clearfloat">
+      <div id="footer-left" class="clearfloat">
+        <?php print $footer_left; ?>
+      </div> 		
+
+      <div id="footer-middle" class="clearfloat">
+        <?php print $footer_middle; ?>
+      </div>
+
+      <div id="footer-right" class="clearfloat">
+        <?php print $footer_right; ?>
+      </div>
+    </div>
+
+    <div id="footer-message">
+	<?php print $footer; ?>
+      <?php print $footer_message; ?>
+
+Copyright 2004-<?php print date("Y"); ?>, <a href="/">Save Ardmore Coalition</a>, 
+A 501(c)(4) Non-Profit Civic Action Organization.<p/>
+
+All content on this site is considered to be released under the <a href="http://en.wikipedia.org/wiki/GNU_Free_Documentation_License" >GNU Free Documentation License</a>.  Blog entries, forum posts, and comments are Copyright by their individual owners and are not endorsed by SAC, its Officers, Directors, or members.
+
+    </div>
+</div>
+<?php print $closure ?>
+</body>
+</html>
