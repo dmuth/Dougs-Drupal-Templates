8ebe36809d7ce4e14a05976f0ad3b1b37e7459e0
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2010-05-12 03:18:25 +0000

    Wrote check_pending_friend_requests() and get_time_offset().
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@35 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- lib/display.inc.php
+++ lib/display.inc.php
@@ -20,3 +20,51 @@ function check_private_messages() {
 
 } // End of check_private_messages()
 
+
+/**
+*
+*/
+function check_pending_friend_requests() {
+
+	if (!module_exists("user_relationships_ui")) {
+		return(null);
+	}
+
+	global $user;
+	_user_relationships_ui_set_notifications($user);
+
+} // End of check_pending_friend_requests()
+
+
+/**
+* Return the number of seconds elapsed since the last call.
+* 
+*/
+function get_time_offset() {
+
+	$retval = 0;
+
+	static $timestamp = "";
+
+	$new_timestamp = microtime(true);
+
+	if (!empty($timestamp)) {
+		$retval = $new_timestamp - $timestamp;
+	}
+
+	$timestamp = $new_timestamp;
+
+	//
+	// Format for 6 digits.
+	//
+	$retval = sprintf("%.6f", $retval);
+
+	return($retval);
+
+} // End of get_time_offset()
+
+//
+// Initialize the timer on include
+//
+get_time_offset();
+
--- page/saveardmore.tpl.php
+++ page/saveardmore.tpl.php
@@ -20,6 +20,12 @@ include($file);
 //
 check_private_messages();
 
+//
+// Do we have any outstandng buddy requests?
+//
+check_pending_friend_requests();
+//print "Timer: " . get_time_offset() . "<br/>\n"; // Debugging
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
