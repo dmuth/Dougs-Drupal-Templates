4ee413f5b28373246abca02b1ef77d631556f71b
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2010-05-12 03:18:23 +0000

    Moved function call and include higher up.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@34 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- page/saveardmore.tpl.php
+++ page/saveardmore.tpl.php
@@ -9,6 +9,17 @@ if (strstr($directory, "-dev")) {
 	print $message;
 }
 
+//
+// Load our functions
+//
+$file = dirname(__FILE__) . "/../lib/display.inc.php";
+include($file);
+
+//
+// If we have unread private messages, let the user know.
+//
+check_private_messages();
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
@@ -38,15 +49,6 @@ var directory = "/<?php print $directory; ?>";
 
 <body>
 
-<?php
-$file = dirname(__FILE__) . "/../lib/display.inc.php";
-include($file);
-check_private_messages();
-//
-// If we have unread private messages, let the user know.
-//
-
-?>
 <table border="0" cellpadding="0" cellspacing="0" id="header">
   <tr>
     <td id="logo">
