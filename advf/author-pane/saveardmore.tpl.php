9dd88def6c8113a6e14b1c8796193e33586afc0b
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2010-05-15 15:36:24 +0000

    Added include() to the author-pane template.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@48 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- advf/author-pane/saveardmore.tpl.php
+++ advf/author-pane/saveardmore.tpl.php
@@ -11,6 +11,17 @@
 
 <?php
 //
+// Load our functions conditionally.
+// The reason for this function_exists() silliness is because touching the
+// lib/display.inc.php file causes the first reload afterward to somwhoe
+// load the file twice.
+//
+if (!function_exists("check_private_messages")) {
+	$file = dirname(__FILE__) . "/../../lib/display.inc.php";
+	include($file);
+}
+
+//
 // Reference to our public profile array
 //
 $public_profile = $profile["Public Profile"];
