490ace0eccb108b5329b429db0ab08b140e66389
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2010-08-18 02:20:17 +0000

    Copied over page template from the PA-Furry set.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@80 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- page/fcn.tpl.php
+++ page/fcn.tpl.php
@@ -1,45 +1,66 @@
 <?php
 //
-// Print a note when we are in the dev site
+// Load our functions conditionally.
+// The reason for this function_exists() silliness is because touching the
+// lib/display.inc.php file causes the first reload afterward to somwhoe
+// load the file twice.
 //
-if (strstr($directory, "dev")) {
-	print "DEVELOPMENT TEMPLATE - If you think you shouldn't be seeing "
-		. "this, please contact <a href=\"/user/2\">Giza</a>.";
+if (!function_exists("check_private_messages")) {
+	$file = dirname(__FILE__) . "/../lib/display.inc.php";
+	include($file);
 }
 
+//print "<pre>"; print_r($GLOBALS); print "</pre>"; // Debugging
+
+//
+// Are we in the dev theme?
+//
+check_dev_theme($directory);
+
+//
+// If we have unread private messages, let the user know.
+//
+check_private_messages();
+
+//
+// Do we have any outstandng buddy requests?
+//
+check_pending_friend_requests();
+//print "Timer: " . get_time_offset() . "<br/>\n"; // Debugging
+
 ?>
-<?php
-// $Id: page.tpl.php,v 1.3 2009/11/11 04:26:12 doug Exp doug $
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
 
 <head>
-  <?php print $head ?>
   <title><?php print $head_title ?></title>
+  <?php print $head ?>
   <?php print $styles ?>
   <?php print $scripts ?>
-  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyle Content in IE */ ?> </script>
 </head>
 
 <body>
 
-<table border="0" cellpadding="0" cellspacing="0" id="header">
+<table border="0" cellpadding="0" cellspacing="0" id="header"
+	>
   <tr>
     <td id="logo">
-	<img src="/<?php print $directory; ?>/images/fcn/header.jpg" />
-      <?php if ($logo) { ?><a href="<?php print $front_page ?>" title="<?php print t('Home') ?>"><img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" /></a><?php } ?>
-      <?php if ($site_name) { ?><h1 class='site-name'><a href="<?php print $front_page ?>" title="<?php print t('Home') ?>"><?php print $site_name ?></a></h1><?php } ?>
+      <?php if ($logo) { ?><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" /></a><?php } ?>
+      <?php if ($site_name) { ?><h1 class='site-name'><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><?php print $site_name ?></a></h1><?php } ?>
       <?php if ($site_slogan) { ?><div class='site-slogan'><?php print $site_slogan ?></div><?php } ?>
     </td>
     <td id="menu">
-      <?php if (isset($secondary_links)) { ?><?php print theme('links', $secondary_links, array('class' => 'links', 'id' => 'subnavlist')) ?><?php } ?>
-      <?php if (isset($primary_links)) { ?><?php print theme('links', $primary_links, array('class' => 'links', 'id' => 'navlist')) ?><?php } ?>
+      <?php if (isset($secondary_links)) { ?><div id="secondary"><?php print theme('links', $secondary_links) ?></div><?php } ?>
+<!--
+      <?php if (isset($primary_links)) { ?><div id="primary"><?php print theme('links', $primary_links) ?></div><?php } ?>
+-->
       <?php print $search_box ?>
     </td>
   </tr>
   <tr>
     <td colspan="2"><div><?php print $header ?></div></td>
   </tr>
+</table>
 <table border="0" cellpadding="0" cellspacing="0" id="content">
   <tr>
     <?php if ($left) { ?><td id="sidebar-left">
@@ -49,12 +70,58 @@ if (strstr($directory, "dev")) {
       <?php if ($mission) { ?><div id="mission"><?php print $mission ?></div><?php } ?>
       <div id="main">
         <?php print $breadcrumb ?>
-        <h1 class="title"><?php print $title ?></h1>
+	<h1 class="title"><?php print $title ?></h1>
+<?php
+//
+// If this is a forum post and the user is not logged in, print
+// a message promting them to do so.
+//
+if (
+	!$GLOBALS["user"]->uid
+	&& !strstr($_REQUEST["q"], "who-is-welcome-here")
+	&& (
+		strstr($_REQUEST["q"], "recent")
+		|| $node->type == "forum"
+		|| $node->type == "event"
+		|| $node->type == "blog"
+	)
+	) {
+	
+	$html = "<div style=\"border: 1px solid black; "
+	//. "font-size: large; "
+	. "padding: 5px; margin: 5px;\">"
+	
+	. "Welcome <b>furry fans</b>!</p>\n"
+	. "We're glad you stopped by.  Go ahead and "
+	. "<a href=\"/user\">register for a free account</a> "
+	. "to get the benefits of being a member, including:\n"
+
+	. "<ul>\n"
+	. "<li>Access to all of our posts and comments</li>\n"
+	. "<li>Your own profile including an avatar, buddy lists, and other social networking features</li>\n"
+	. "<li>The ability to send private messages to other users on this site</li>\n"
+	. "<li>The ability to chat and interact with other furries in and around Pennsylvania.</li>\n"
+	. "</ul>\n"
+
+	. "<span style=\"font-size: larger; \">"
+	. "Creating an account is easy. <a href=\"/user\">Register now!</a>"
+	. "</span>\n"
+	. "<br/>\n"
+	. "<br/>\n"
+
+	. "(Not a furry fan?  That's cool.  <a href=\"/who-is-welcome-here\">You're still welcome here.</a>)"
+	
+	. "</div>";
+		
+	print $html;
+
+}
+
+?>
         <div class="tabs"><?php print $tabs ?></div>
-        <?php if ($show_messages) { print $messages; } ?>
         <?php print $help ?>
+        <?php print $messages ?>
         <?php print $content; ?>
-        <?php print $feed_icons; ?>
       </div>
     </td>
     <?php if ($right) { ?><td id="sidebar-right">
@@ -63,9 +130,30 @@ if (strstr($directory, "dev")) {
   </tr>
 </table>
 
+
+<a name="footer"></a>
 <div id="footer">
-  <?php print $footer_message ?>
-  <?php print $footer ?>
+
+    <div id="footer-region" class="clearfloat">
+      <div id="footer-left" class="clearfloat">
+        <?php print $footer_left; ?>
+      </div> 		
+
+      <div id="footer-middle" class="clearfloat">
+        <?php print $footer_middle; ?>
+      </div>
+
+      <div id="footer-right" class="clearfloat">
+        <?php print $footer_right; ?>
+      </div>
+    </div>
+
+    <div id="footer-message">
+	<?php print $footer; ?>
+  	<?php print $footer_message ?><p>
+
+    </div>
+
 </div>
 <?php print $closure ?>
 </body>
