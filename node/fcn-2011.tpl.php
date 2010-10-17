2124a1949212150f70be6a35e5ee3d494fb42cf6
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2010-10-17 20:47:03 +0000

    First import of FCN 2011 styles and pages.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@100 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

---
+++ node/fcn-2011.tpl.php
@@ -0,0 +1,93 @@
+<?php
+//
+// Load our functions conditionally.
+// The reason for this function_exists() silliness is because touching the
+// lib/display.inc.php file causes the first reload afterward to somwhoe
+// load the file twice.
+//
+if (!function_exists("check_private_messages")) {
+	$file = dirname(__FILE__) . "/../lib/display.inc.php";
+	include($file);
+}
+
+?>
+<div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
+<?php 
+if ($page == 0) { 
+	?>
+	<h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2>
+	<?php 
+}; 
+?>
+    
+<span class="submitted"><?php 
+
+//
+// Load profile data for this user
+//
+profile_load_profile($node);
+
+//
+// Grab their public display name, if present.
+//
+$name = "";
+if (!empty($node->profile_display_name)) {
+	$name = " (" . $node->profile_display_name . ")";
+}
+
+//
+// Glue in their name
+//
+$submitted = "Submitted by " . l($node->name, "user/" . $node->uid)
+	. $name
+	. " on "
+	. format_date($node->created)
+	;
+
+	print $submitted;
+	?></span>
+<span class="taxonomy">
+<?php
+if (!empty($terms)) {
+	print "Tags: " . $terms;
+}
+
+?></span>
+
+<table border=0>
+<tr>
+<td valign="top">
+<?php 
+
+if ($picture) {
+	//
+	// Print the picture ONLY if this is a forum post and not a "teaser",
+	// which means an abbreciated post shown on the front page.
+	//
+	if (
+		($node->type == "forum" 
+			//|| $node->type == "page"
+			//|| $node->type == "blog"
+			//|| $node->type == "event"
+			)
+		&& empty($node->teaser)
+		&& empty($node->sticky)
+		) {
+			//print $picture;
+			$account = user_load($node->uid);
+			$template = "advf-author-pane";
+			$author_pane = theme('author_pane', $account, advanced_forum_path_to_images(), $template);
+			print $author_pane;
+	}
+}
+?>
+</td>
+<td valign="top">
+<div class="content"><?php print $content?></div>
+</td>
+</tr>
+
+</table>
+
+    <?php if ($links) { ?><div class="links">&raquo; <?php print $links?></div><?php }; ?>
+  </div>
---
+++ page/fcn-2011.tpl.php
@@ -0,0 +1,141 @@
+<?php
+//
+// Load our functions conditionally.
+// The reason for this function_exists() silliness is because touching the
+// lib/display.inc.php file causes the first reload afterward to somwhoe
+// load the file twice.
+//
+if (!function_exists("check_private_messages")) {
+	$file = dirname(__FILE__) . "/../lib/display.inc.php";
+	include($file);
+}
+
+//print "<pre>"; print_r($GLOBALS); print "</pre>"; // Debugging
+
+//
+// Are we in the dev theme?
+//
+check_dev_theme($directory);
+
+//
+// If we have unread private messages, let the user know.
+//
+check_private_messages();
+
+//
+// Do we have any outstandng buddy requests?
+//
+check_pending_friend_requests();
+//print "Timer: " . get_time_offset() . "<br/>\n"; // Debugging
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
+
+<head>
+  <title><?php print $head_title ?></title>
+  <?php print $head ?>
+  <?php print $styles ?>
+  <?php print $scripts ?>
+</head>
+
+<body style="background:url(/<?php print $directory; ?>/images/fcn/gradient.gif" /); background-repeat: repeat-x; background-position: top">
+
+<table border="0" cellpadding="0" cellspacing="0" id="header"
+	>
+  <tr>
+    <td id="logo_left">
+		<img src="/<?php print $directory; ?>/images/fcn/header_left.gif" />
+		</td>
+    <td id="logo">
+    <img src="/<?php print $directory; ?>/images/fcn/header.gif" />
+      <?php if ($logo) { ?><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" /></a><?php } ?>
+      <?php if ($site_name) { ?><h1 class='site-name'><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><?php print $site_name ?></a></h1><?php } ?>
+      <?php if ($site_slogan) { ?><div class='site-slogan'><?php print $site_slogan ?></div><?php } ?>
+    </td>
+	<td id="logo_right">
+		<img src="/<?php print $directory; ?>/images/fcn/header_right.gif" />
+		</td>
+    <td id="menu">
+      <?php if (isset($secondary_links)) { ?><div id="secondary"><?php print theme('links', $secondary_links) ?></div><?php } ?>
+<!--
+      <?php if (isset($primary_links)) { ?><div id="primary"><?php print theme('links', $primary_links) ?></div><?php } ?>
+-->
+      <?php print $search_box ?>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2"><div><?php print $header ?></div></td>
+  </tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" id="content">
+  <tr>
+    <?php if ($left) { ?><td id="sidebar-left">
+      <?php print $left ?>
+    </td><?php } ?>
+    <td valign="top">
+      <?php if ($mission) { ?><div id="mission"><?php print $mission ?></div><?php } ?>
+      <div id="main">
+        <?php print $breadcrumb ?>
+	<h1 class="title"><?php print $title ?></h1>
+<?php
+//
+// If this is a forum post and the user is not logged in, print
+// a message promting them to do so.
+//
+if (
+	!$GLOBALS["user"]->uid
+	&& !strstr($_REQUEST["q"], "who-is-welcome-here")
+	&& (
+		strstr($_REQUEST["q"], "recent")
+		|| $node->type == "forum"
+		|| $node->type == "event"
+		|| $node->type == "blog"
+	)
+	) {
+	
+	//print $html;
+
+}
+
+?>
+        <div class="tabs"><?php print $tabs ?></div>
+        <?php print $help ?>
+        <?php print $messages ?>
+        <?php print $content; ?>
+      </div>
+    </td>
+    <?php if ($right) { ?><td id="sidebar-right">
+      <?php print $right ?>
+    </td><?php } ?>
+  </tr>
+</table>
+
+
+<a name="footer"></a>
+<div id="footer">
+
+    <div id="footer-region" class="clearfloat">
+      <div id="footer-left" class="clearfloat">
+        <?php print $footer_left; ?>
+      </div> 		
+
+      <div id="footer-middle" class="clearfloat">
+        <?php print $footer_middle; ?>
+      </div>
+
+      <div id="footer-right" class="clearfloat">
+        <?php print $footer_right; ?>
+      </div>
+    </div>
+
+    <div id="footer-message">
+	<?php print $footer; ?>
+  	<?php print $footer_message ?><p>
+
+    </div>
+
+</div>
+<?php print $closure ?>
+</body>
+</html>
