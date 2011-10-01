702d53a05c5bdd1b53006747b7416b4bf065e041
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2011-10-01 21:12:43 +0000

    Added Open Graph tag.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@173 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- page/anthrocon-2011.tpl.php
+++ page/anthrocon-2011.tpl.php
@@ -45,7 +45,7 @@ check_pending_friend_requests();
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 
 <head>
   <title><?php print $head_title ?></title>
