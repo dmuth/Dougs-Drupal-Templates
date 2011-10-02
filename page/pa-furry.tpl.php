ab19157a7eab37961f5448a817b3882c2b2eef59
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2011-10-02 00:20:46 +0000

    Added Open Graph meta tag.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@179 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- page/pa-furry.tpl.php
+++ page/pa-furry.tpl.php
@@ -30,7 +30,7 @@ check_pending_friend_requests();
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
 
 <head>
   <title><?php print $head_title ?></title>
