fb6141aa2b041861c61c7598268e59170a952826
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2011-10-09 02:30:26 +0000

    Added Facebook Like button and Google Plus One button.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@201 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- page/southardmore.tpl.php
+++ page/southardmore.tpl.php
@@ -84,7 +84,59 @@ var directory = "/<?php print $directory; ?>";
     <td valign="top">
       <div id="main">
         <?php print $breadcrumb ?>
-        <h1 class="title"><?php print $title ?></h1>
+	<?php
+	//
+	// Facebook code.
+	//
+	$url = $GLOBALS["base_url"] . request_uri();
+	$url_string = rawurlencode($url);
+	$fb_url = "http://www.facebook.com/plugins/like.php?"
+		. "href=${url_string}&amp;"
+		. "layout=button_count&amp;show_faces=true&amp;action=like&amp;font&amp;colorscheme=light"
+		;
+	$fb_html = "<iframe src=\"" . $fb_url . "\""
+			. "scrolling=\"no\" frameborder=\"0\" "
+			. "style=\"border:none; overflow:hidden; width:90px; "
+				. "height: 40px; float: right; "
+				. "padding-top: 10px; \" "
+			. "allowTransparency=\"true\">"
+			. "</iframe>"
+			;
+
+	$plus_one = ""
+		. "<g:plusone size=\"medium\"></g:plusone>\n"
+		. "<script type=\"text/javascript\">\n"
+		. "(function() {\n"
+			. "var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;\n"
+			. "po.src = 'https://apis.google.com/js/plusone.js';\n"
+			. "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);\n"
+		. "})();\n"
+		. "</script>\n"
+		;
+
+	$plus_one_html = ""
+		. "<span style=\"float: right; padding-top: 10px; \">"
+		. $plus_one
+		. "</span>"
+		;
+	?>
+	<h1 class="title"><?php print $title ?>
+		<?php
+		//
+		// If we are running under Wamp, that only happens
+		// for onsite reg, so suppres the Facebook Like 
+		// button there. 	
+		//
+		if (
+			!strstr($_SERVER["DOCUMENT_ROOT"], "/wamp/")
+			&& (arg(0) != "admin")
+			) {
+			print $fb_html;
+			print $plus_one_html;
+		}
+
+		?>
+	</h1>
 <?php
 //
 // If this is a forum post and the user is not logged in, print
