ee2f66026505392c02705b61912a9cbb15ee6a0c
doug.muth <doug.muth@72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25>
2011-10-09 02:26:33 +0000

    Include functions and check for dev template.
    git-svn-id: https://dougs-drupal-templates.googlecode.com/svn/trunk@200 72c7b6c0-8d8e-a866-83cd-a0dcbd31ac25

--- page/southardmore.tpl.php
+++ page/southardmore.tpl.php
@@ -1,13 +1,19 @@
 <?php
 //
-// Print a note when we are in the dev site
+// Load our functions conditionally.
+// The reason for this function_exists() silliness is because touching the
+// lib/display.inc.php file causes the first reload afterward to somwhoe
+// load the file twice.
 //
-if (strstr($directory, "-dev")) {
-	$message = "DEVELOPMENT TEMPLATE - If you think you shouldn't be seeing "
-		. "this, please <a href=\"/contact\">contact us</a>.";
-	$head_title .= " - " . $message;
-	print $message;
+if (!function_exists("check_private_messages")) {
+	$file = dirname(__FILE__) . "/../lib/display.inc.php";
+	include($file);
 }
+
+//
+// Are we in the dev theme?
+//
+check_dev_theme($directory);
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
