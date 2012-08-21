7199ebda2505a9fe15a5ac68ed9516f97c9fb950
Douglas Muth <dmuth@dmuth.org>
2012-08-20 20:44:55 -0400

    Got updated headers.  Started working on primary and secondary menus.

--- page/fcn-2013.tpl.php
+++ page/fcn-2013.tpl.php
@@ -49,21 +49,43 @@ $image_dir = "/" . $directory . "/images/fcn-2013";
 <body style="background: #0b3926; background-repeat: repeat-x; background-position: top">
 
 <span id="sidebar-left" 
-	style="background: url(<?php print $image_dir . "/background-left.gif"; ?>) no-repeat; float: left; height: 600px; width:130px; "
+	style="background: url(<?php print $image_dir . "/background-left.gif"; ?>) no-repeat; float: left; height: 1500px; width:130px; "
 	></span>
 
-<span id="fcn-header" style="float: left; ">
+<span id="fcn-header" style="float: left; background-color: white; ">
 
-<?php if (isset($secondary_links)) { ?><div id="secondary"><?php print theme('links', $secondary_links) ?></div><?php } ?>
-<?php if (isset($primary_links)) { ?><div id="primary"><?php print theme('links', $primary_links) ?></div><?php } ?>
 
-<img src="<?php print $image_dir . "/header-left.gif"; ?>" style=" " 
-	/><img src="<?php print $image_dir . "/header-right.gif"; ?>" style=" " 
-	/><br/><div id="sidebar-left" style="float: left; ">
+<div id="fcn-header-graphics" style="padding: 0; " >
+<?php // The set of two header graphics and Menus ?>
+<div style="float: left; ">
+<img src="<?php print $image_dir . "/header-left.gif"; ?>" style=" " />
+</div>
+<div style="float: left; ">
+<div id="fcn-links-primary" style="color: white; background-color: black; width: 100%; height: 29px; ">
+<?php print theme('links', $primary_links) ?>
+</div><?php // fcn-links-primary ?>
+
+<div style="height: 174px; ">
+<img src="<?php print $image_dir . "/header-right.gif"; ?>" style=" " />
+</div>
+
+<div id="fcn-links-secondary" style="color: white; background-color: black; width: 100%; height: 32px; ">
+April 12 - 14, 2013 Detroit Novi Sheraton
+<?php print theme('links', $secondary_links) ?>
+</div> <?php // fcn-links-secondary ?>
+
+</div>
+</div>
+<br clear="all" />
+
+<?php // The center part of the site, including the left sidebar ?>
+<div id="fcn-center" 
+	><div id="sidebar-left" style="float: left; ">
 <?php print $left; ?>
 </div><?php // sidebar-left ?>
 
-<div id="fcn-main" style="background-color: white; width: 950px; ">
+
+<div id="fcn-main" style="background-color: white; float: left; width: 763px; ">
 <?php
 
 include_once("fcn-2013/main.tpl.php");
@@ -73,10 +95,12 @@ include_once("fcn-2013/footer.tpl.php");
 ?>
 </div><?php // fcn-main ?>
 
+</div><?php // fcn-center ?>
+
 </span><?php // fcn-header ?>
 
 <span id="sidebar-right" 
-	style="background: url(<?php print $image_dir . "/background-right.gif"; ?>) no-repeat; float: left; height: 600px; width: 130px; "
+	style="background: url(<?php print $image_dir . "/background-right.gif"; ?>) no-repeat; float: left; height: 1500px; width: 130px; "
 	></span>
 
 <br clear="all" />
